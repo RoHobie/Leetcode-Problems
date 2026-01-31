3d1d9b2eba47185b38d4f2408af23b3108dc9e0c
Ronit Thakur <ronitthakur267@gmail.com>
2026-01-31 10:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.84%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x<2) return x;
+        if (x < 2)
+            return x;
         int low = 1;
         int high = x / 2;
         int ans = 0;
