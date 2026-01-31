de1bc031ea297aff6642dafd87a9e4181f749a8c
Ronit Thakur <ronitthakur267@gmail.com>
2026-01-31 13:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (56.07%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        if(c < 2){
+            return true;
+        }
+        int low = 1;
+        int high = c/2;
+        long long closest = 0;
+
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(mid <= c/mid){
+                closest = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        
+        int left = 0;
+        while(left<=closest){
+            long long product = left*left + closest*closest;
+            if(product == c){
+                return true;
+            }
+            if(product < c){
+                left++;
+            }else{
+                closest--;
+            }
+        }
+
+        return false;
+    }
+};
