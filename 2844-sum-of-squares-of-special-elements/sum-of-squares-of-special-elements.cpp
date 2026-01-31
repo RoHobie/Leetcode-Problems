356dbce90ce26d8ed6134ebdbb4b477ff2709a30
Ronit Thakur <ronitthakur267@gmail.com>
2026-01-31 13:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (40.17%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
@@ -3,9 +3,9 @@ public:
     int sumOfSquares(vector<int>& nums) {
         long long square = 0;
         int size = nums.size();
-        for(int i = 0; i<size; i++){
-            if(size%(i+1) == 0){
-                square += nums[i]*nums[i];
+        for (int i = 0; i < size; i++) {
+            if (size % (i + 1) == 0) {
+                square += nums[i] * nums[i];
             }
         }
         return square;
